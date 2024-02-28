7a7ad59266fde410edd3fd7745a6bf1285ba2882
MarcosIsmael <marcosgaliano96@gmail.com>
2024-02-27 21:45:19 -0300

    react icons

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
+import { IoLogoReact } from 'react-icons/io5'
 
 export const Sidebar = () => {
   return (
     <div id="menu" style={{width:'400px'}} className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
     
     <div id="logo" className="my-4 px-6">
-     <h1 className="text-lg md:text-2xl font-bold text-white">Dash<span className="text-blue-500">8</span>.</h1>
+     <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
+    <IoLogoReact className='mr-2'/>
+        <span>Dash</span>
+        <span className="text-blue-500">8</span>
+        .</h1>
      <p className="text-slate-500 text-sm">Manage your actions and activities</p>
     </div>
     <div id="profile" className="px-6 py-10">
