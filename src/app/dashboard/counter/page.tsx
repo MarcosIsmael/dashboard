49aa5de2940a54bf24d13b0b29d08d4eaf80beac
MarcosIsmael <marcosgaliano96@gmail.com>
2024-03-01 16:41:35 -0300

    separando codigo para meta datos

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,19 @@
+import { CounterCart } from "@/app/shopping-cart/components";
+
+
+
+export const metadata ={
+  title: 'Sopping cart',
+  description:'Un simple contador'
+}
+
 
 export default function CounterPage() {
+
   return (
-    <div>
-      <h1>CounterPage</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CounterCart value={20}/>
     </div>
   );
 }
